f9ee74661884181c6ad5269df24b9c397d96a5f8
Karl Bartel <karl42@gmail.com>
2019-08-22 21:18:55 +0200

    enable foreign key checks

--- iprefer/db.py
+++ iprefer/db.py
@@ -16,6 +16,7 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
         db.execute(f"ATTACH DATABASE '{USER_DATABASE}' AS user")
+        db.execute("PRAGMA foreign_keys = ON")
     return db
 
 
